3a1451e3d3f11e13da60db724126d58fff001c54
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-19 09:22:38 +0300

    create portal and applied it to Modal

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,13 @@
+import s from './Header.module.css'
+import {Cart} from '../Cart/Cart.tsx';
+
+export const Header = () => {
+    return (
+        <div className={s.headerWrapper}>
+            <div className={s.container}>
+                <h3>logotype</h3>
+                <Cart />
+            </div>
+        </div>
+    )
+}
