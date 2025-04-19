974bddc3757b6716ed24755f3cce4ad7b78827d3
AliakseiKrasko <alekskrasko1985@gmail.com>
2025-04-19 17:30:58 +0300

    added colors in Button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,20 @@
-import {ReactNode} from 'react';
+import { ComponentPropsWithoutRef } from 'react'
+import s from './Button.module.css'
 
 type Props = {
-    children: ReactNode
-    onClick?: () => void
-    title?: string
-    type?: 'button' | 'submit' | 'reset'
-};
-export const Button = ( { children, onClick, type, title } : Props) => {
+    variant?: 'primary' | 'secondary' | 'outlined'
+} & ComponentPropsWithoutRef<'button'>
+
+export const Button = ({ variant = 'primary', className, ...rest }: Props) => {
     return (
-        <button onClick={onClick} title={title} type={type}  >
-            {children}
-        </button>
-    );
-};
+        <button
+            className={`${s.button}
+  ${variant === 'primary' ? s.primary : ''}
+  ${variant === 'secondary' ? s.secondary : ''}
+  ${variant === 'outlined' ? s.outlined : ''}
+  ${className}
+     `}
+            {...rest}
+        />
+    )
+}
